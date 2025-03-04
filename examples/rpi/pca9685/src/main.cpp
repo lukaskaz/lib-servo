b0181923078e4e408ca43f915703557acd19d1b5
Lukasz Kazmierczak <lukask84@o2.pl>
2025-03-04 15:43:45 +0100

    Aligning servo with changes in liblogger
    Signed-off-by: Lukasz Kazmierczak <lukask84@o2.pl>

--- examples/rpi/pca9685/src/main.cpp
+++ examples/rpi/pca9685/src/main.cpp
@@ -1,4 +1,4 @@
-#include "log/interfaces/console.hpp"
+#include "log/interfaces/console/logs.hpp"
 #include "servo/interfaces/group/servo.hpp"
 #include "servo/interfaces/rpi/pca9685/servo.hpp"
 
@@ -18,15 +18,15 @@ int main(int argc, char** argv)
                 << "First scenario -> moving all servos to given position\n";
             const auto amount = (int32_t)atoi(argv[1]);
             const auto pospct = (double)atoi(argv[2]);
-            const auto loglvl = (bool)atoi(argv[3]) ? logging::type::debug
-                                                    : logging::type::info;
+            const auto loglvl =
+                (bool)atoi(argv[3]) ? logs::level::debug : logs::level::info;
             const auto driverpath =
                 argc < 5 ? "/sys/class/pwm/pwmchip2/" : argv[4];
             auto sequence = std::views::iota(0, amount);
             std::vector<std::shared_ptr<servo::ServoIf>> servos;
 
-            auto logif =
-                logging::LogFactory::create<logging::console::Log>(loglvl);
+            auto logif = logs::Factory::create<logs::console::Log,
+                                               logs::console::config_t>(loglvl);
             std::ranges::for_each(
                 sequence, [&driverpath, &servos, logif](uint8_t num) {
                     using namespace servo::rpi::pca9685;
